610a20a9258951288c37e97c2f1ddd3c74449806
SeniorCod-72 <wwekesa72@gmail.com>
2025-04-01 12:25:02 +0300

    Flask api implemented in app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+db = SQLAlchemy()
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///heroes.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] =False
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
